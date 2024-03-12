7ae7813ac32d7aa6c927ebbe0e0f9afacc55b8c4
Cheryl Lee <clee0937@gmail.com>
2024-03-12 15:48:31 +0000

    added calculation functions

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const calculatorNumbers = document.querySelectorAll<HTMLButtonElement>(
 const calculatorOperators = document.querySelectorAll<HTMLButtonElement>(
   ".calculator__operator"
 );
-const AC = document.querySelector<HTMLButtonElement>(".calculator__ac");
+const ac = document.querySelector<HTMLButtonElement>(".calculator__ac");
 const plus_minus = document.querySelector<HTMLButtonElement>(
   ".calculator__plus-minus"
 );
@@ -32,7 +32,7 @@ if (calculatorOperators.length === 0) {
 
 if (
   !calculatorDisplay ||
-  !AC ||
+  !ac ||
   !plus_minus ||
   !percentage ||
   !equals ||
@@ -43,18 +43,82 @@ if (
 
 // Calculator screen
 calculatorDisplay.innerText = "0";
+let previousNumber: string = "";
+let currentNumber: string = "";
+let selectedOperator: string = "";
 
 // Event handlers
-const updateDisplay = (event: Event) => {
+
+const handleButtonClick = (event: Event) => {
   if (calculatorDisplay.innerText.length === 9) return;
 
-  const target = event.target as HTMLElement;
+  if (calculatorDisplay.innerText.length >= 6) {
+    calculatorDisplay.classList.add("calculator__display--small");
+  }
+
+  updateDisplay(event);
+};
+
+const updateDisplay = (event: Event) => {
+  const target = event.target as HTMLButtonElement;
 
   if (calculatorDisplay.innerText === "0") {
     calculatorDisplay.innerText = "";
   }
+  currentNumber += target.innerText;
+  calculatorDisplay.innerText = currentNumber;
+};
 
-  calculatorDisplay.innerText += target.innerText;
+const addToSelectedOperator = (event: Event) => {
+  const target = event.target as HTMLButtonElement;
+
+  if (
+    target.innerText !== selectedOperator &&
+    previousNumber &&
+    currentNumber
+  ) {
+    calculate(selectedOperator);
+    selectedOperator = target.innerText;
+    return;
+  }
+
+  selectedOperator = target.innerText;
+
+  if (previousNumber && currentNumber) {
+    calculate(selectedOperator);
+  } else {
+    if (currentNumber) {
+      previousNumber = currentNumber;
+      currentNumber = "";
+    }
+  }
+};
+
+const calculate = (operator: string) => {
+  let result: number = 0;
+  const current = parseFloat(currentNumber);
+  const previous = parseFloat(previousNumber);
+  if (isNaN(current) || isNaN(previous)) return;
+
+  if (operator === "+") {
+    result = previous + current;
+  } else if (operator === "−") {
+    result = previous - current;
+  } else if (operator === "×") {
+    result = previous * current;
+  } else if (operator === "÷") {
+    result = previous / current;
+  }
+
+  calculatorDisplay.innerText = result.toString();
+  previousNumber = result.toString();
+  currentNumber = "";
+  return previousNumber;
+};
+
+const returnResult = () => {
+  if (!previousNumber || !currentNumber) return;
+  calculatorDisplay.innerText = calculate(selectedOperator) as string;
 };
 
 // }
@@ -62,5 +126,11 @@ const updateDisplay = (event: Event) => {
 // 3. create forEach loop to attach event handlers to number buttons
 
 calculatorNumbers.forEach((number) => {
-  number.addEventListener("click", updateDisplay);
+  number.addEventListener("click", handleButtonClick);
 });
+
+calculatorOperators.forEach((operator) => {
+  operator.addEventListener("click", addToSelectedOperator);
+});
+
+equals.addEventListener("click", returnResult);
